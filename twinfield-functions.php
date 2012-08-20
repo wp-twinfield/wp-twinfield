40575ca4f523ed1b4e4cf87b9df45f7fab6be498
Remco Tolsma <info@remcotolsma.nl>
2012-08-20 22:09:56 +0200

    Added some handy functions.

---
+++ twinfield-functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function twinfield_get_offices() {
+	
+}
+
+function twinfield_get_debitors() {
+	
+}
+
+function twinfield_get_sales_invoice( $id ) {
+	
+}
+
+function twinfield_get_transaction( $number, $code, $office ) {
+	/*
+	<?xml version="1.0"?>
+	<read>
+		<type>transaction</type>
+		<office>11024</office>
+		<code>VRK</code>
+		<number>201100001</number>
+	</read>
+	*/
+}
+
+function twinfield_get_debtor( $id ) {
+	
+}
+
