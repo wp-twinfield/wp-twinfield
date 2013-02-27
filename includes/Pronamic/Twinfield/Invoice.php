16497fa7dffc9e63ccdf27ae34edcdccef60bc82
Remco Tolsma <info@remcotolsma.nl>
2013-02-27 21:39:56 +0100

    Added invoice class and documentation.

---
+++ includes/Pronamic/Twinfield/Invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pronamic\Twinfield;
+
+/**
+ * Title: Invoice
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Invoice {
+	/**
+	 * Invoice code for an 'factuur' invoice
+	 * 
+	 * @var string
+	 */
+	const CODE_FACTUUR = 'factuur';
+
+	/**
+	 * Invoice code for an 'uren' invoice
+	 * 
+	 * @var string
+	 */
+	const CODE_UREN = 'uren';
+}
