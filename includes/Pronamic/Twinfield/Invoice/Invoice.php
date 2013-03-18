118de003b0c0327e707cc02239f02be58e4fdb12
Leon Rowland <leon@rowland.nl>
2013-03-18 20:52:37 +0100

    More comments

--- includes/Pronamic/Twinfield/Invoice/Invoice.php
+++ includes/Pronamic/Twinfield/Invoice/Invoice.php
@@ -2,46 +2,147 @@
 
 namespace Pronamic\Twinfield\Invoice;
 
+/**
+ * Invoice Class
+ *
+ * Is an object for mapping data from a response ( or making
+ * a request ).
+ *
+ * It is normally passed into the Element class to convert it
+ * into the XML format.
+ *
+ * @since 0.0.1
+ *
+ * @package Pronamic\Twinfield
+ * @subpackage Invoice
+ * @author Leon Rowland <leon@rowland.nl>
+ * @copyright (c) 2013, Leon Rowland
+ * @version 0.0.1
+ */
 class Invoice {
+	/**
+	 * A customer for this invoice
+	 *
+	 * @access private
+	 * @var \Pronamic\Customer\Customer
+	 */
 	private $customer;
 
-	private $invoiceType;
+	/**
+	 * The type of invoice this
+	 * object is
+	 *
+	 * @access private
+	 * @var string
+	 */
+	private $type;
 
-	private $orders;
+	/**
+	 * Holds the invoice lines for this
+	 * invoice.
+	 *
+	 * @see \Pronamic\Twinfield\Invoice\InvoiceLine()
+	 *
+	 * @access private
+	 * @var array of InvoiceLine
+	 */
+	private $lines;
 
-	public function addOrder(InvoiceLine $order) {
-		$this->orders[$order->getID()] = $order;
+	/**
+	 * Adds a line to this invoice.
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @param \Pronamic\Twinfield\Invoice\InvoiceLine $order
+	 * @return \Pronamic\Twinfield\Invoice\Invoice
+	 */
+	public function addLine(InvoiceLine $order) {
+		$this->lines[$order->getID()] = $order;
 		return $this;
 	}
 
-	public function removeOrder( $uid ) {
-		if ( array_key_exists( $uid, $this->orders ) ) {
-			unset( $this->orders[$uid] );
+	/**
+	 * Removes a line from this invoice
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @param string $uid The unique key for this order
+	 * @return boolean If the line was removed
+	 */
+	public function removeLine( $uid ) {
+		if ( array_key_exists( $uid, $this->lines ) ) {
+			unset( $this->lines[$uid] );
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public function getOrders() {
-		return $this->orders;
+	/**
+	 * Returns all lines currently assigned
+	 * to this invoice
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @return array of InvoiceLine
+	 */
+	public function getLines() {
+		return $this->lines;
 	}
 
+	/**
+	 * Gets the current assigned customer
+	 * for this invoice
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @return \Pronamic\Twinfield\Customer\Customer
+	 */
 	public function getCustomer() {
 		return $this->customer;
 	}
 
+	/**
+	 * Sets the customer for this invoice
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @param \Pronamic\Twinfield\Customer\Customer $customer
+	 * @return \Pronamic\Twinfield\Invoice\Invoice
+	 */
 	public function setCustomer( \Pronamic\Twinfield\Customer\Customer $customer ) {
 		$this->customer = $customer;
 		return $this;
 	}
 
-	public function getInvoiceType() {
-		return $this->invoiceType;
+	/**
+	 * Returns the set invoice type
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getType() {
+		return $this->type;
 	}
 
-	public function setInvoiceType( $invoiceType ) {
-		$this->invoiceType = $invoiceType;
+	/**
+	 * Sets the type for this invoice
+	 *
+	 * @since 0.0.1
+	 *
+	 * @access public
+	 * @param string $invoiceType
+	 * @return \Pronamic\Twinfield\Invoice\Invoice
+	 */
+	public function setType( $invoiceType ) {
+		$this->type = $invoiceType;
 		return $this;
 	}
 }
