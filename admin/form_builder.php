d3cb493b4c0512fb9e263e7afe6b25f1a31b86f0
Leon Rowland <leon@rowland.nl>
2013-03-20 08:41:35 +0100

    update to use wordpress base styling

--- admin/form_builder.php
+++ admin/form_builder.php
@@ -1,32 +1,27 @@
 <?php
-
 $form_builder = new \Pronamic\WP\FormBuilder\FormBuilder();
 
-if ( isset( $_GET['twinfield_form'] ) ) {
-	$page_form = $_GET['twinfield_form'];
+if ( isset( $_GET[ 'twinfield_form' ] ) ) {
+	$page_form = $_GET[ 'twinfield_form' ];
 } else {
 	$page_form = '';
 }
-
 ?>
 <div class="wrap">
 	<?php screen_icon(); ?>
-	<h2><?php echo get_admin_page_title(); ?></h2>
-
-	<ul class="twinfield_form_types_list">
-
+	<h2 class="nav-tab-wrapper">
+		<a class="nav-tab <?php if ( empty( $page_form ) ): ?> nav-tab-active <?php endif; ?>" href="<?php echo admin_url( 'admin.php?page=twinfield_form_builder' ); ?>"><?php echo get_admin_page_title(); ?></a>
 		<?php foreach ( $form_builder->getValidForms() as $type ) : ?>
-		<li>
-			<a class="<?php echo ( $page_form == $type ? 'selected' : '' ); ?>" href="<?php echo twinfield_get_form_action( $type ); ?>"><?php echo ucfirst( $type ); ?></a>
-		</li>
+			<a class="nav-tab <?php echo ( $page_form == $type ? 'nav-tab-active' : '' ); ?>" href="<?php echo twinfield_get_form_action( $type ); ?>"><?php echo ucfirst( $type ); ?></a>
 		<?php endforeach; ?>
-	</ul>
+	</h2>
+
 	<?php $form_builder->create_form(); ?>
 
 	<form action="<?php echo twinfield_get_form_action( 'invoice' ); ?>" method="post">
 		<input type="hidden" name="customerID" value="1002"/>
 		<input type="hidden" name="lines[1][article]" value="123"/>
 		<input type="hidden" name="lines[2][article]" value="456" />
-		<?php submit_button('test submit'); ?>
+		<?php submit_button( 'test submit' ); ?>
 	</form>
 </div>
