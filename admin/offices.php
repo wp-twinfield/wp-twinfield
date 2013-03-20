a7b8a38103dc2a7a906af52cb7577ab13b46a60b
Remco Tolsma <info@remcotolsma.nl>
2013-03-20 16:37:30 +0100

    Use get_admin_page_title() on the admin pages.

--- admin/offices.php
+++ admin/offices.php
@@ -1,9 +1,7 @@
 <div class="wrap">
 	<?php screen_icon( 'twinfield' ); ?>
 
-	<h2>
-		<?php echo esc_html( 'Twinfield Offices' ); ?>
-	</h2>
+	<h2><?php echo get_admin_page_title(); ?></h2>
 
 	<?php 
 
