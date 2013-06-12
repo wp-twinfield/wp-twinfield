fb68c7b7db1db56e635709883425d9a9db8f29f9
Leon Rowland <leon@rowland.nl>
2013-06-12 09:56:50 +0200

    addition of labels

--- views/Pronamic/WP/Article/articlemetabox_view.php
+++ views/Pronamic/WP/Article/articlemetabox_view.php
@@ -1,11 +1,11 @@
 <?php echo $nonce; ?>
 <table class="form-table">
 	<tr>
-		<th><?php _e( 'Article ID', 'twinfield' ); ?></th>
-		<td><input type="text" name="twinfield_article_id" value="<?php echo $twinfield_article_id; ?>" /></td>
+		<th><label for="twinfield_article_id"><?php _e( 'Article ID', 'twinfield' ); ?></label></th>
+		<td><input type="text" id="twinfield_article_id" name="twinfield_article_id" value="<?php echo $twinfield_article_id; ?>" /></td>
 	</tr>
 	<tr>
-		<th><?php _e( 'Subarticle ID', 'twinfield' ); ?></th>
-		<td><input type="text" name="twinfield_subarticle_id" value='<?php echo $twinfield_subarticle_id; ?>' /></td>
+		<th><label for="twinfield_subarticle_id"><?php _e( 'Subarticle ID', 'twinfield' ); ?></label></th>
+		<td><input type="text" id="twinfield_subarticle_id" name="twinfield_subarticle_id" value='<?php echo $twinfield_subarticle_id; ?>' /></td>
 	</tr>
 </table>
