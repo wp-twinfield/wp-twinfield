52e234388c56f503e54aeca5c666717060e03e72
Leon Rowland <leon@rowland.nl>
2013-06-20 15:23:26 +0200

    Severe minimalization of class. More streamlined and supports more classes without the need to specify in multiple arrays. See more at the FormBuilderFactory

--- includes/Pronamic/WP/Twinfield/FormBuilder/FormBuilder.php
+++ includes/Pronamic/WP/Twinfield/FormBuilder/FormBuilder.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Pronamic\WP\Twinfield\FormBuilder;
 /**
  * FormBuilder Class
  *
- * Used to create forms, and listen for inputs.  Will
- * load the right classes depending on chosen form
+ * Used to create forms, and listen for inputs. 
+ * 
+ * Uses the FormBuilderFactory
  *
  * @since 0.0.1
  *
@@ -15,32 +15,9 @@ namespace Pronamic\WP\Twinfield\FormBuilder;
  * @copyright (c) 2013, Leon Rowland
  * @version 0.0.1
  */
-use \ZFramework\Base\View;
 
+namespace Pronamic\WP\Twinfield\FormBuilder;
 class FormBuilder {
-
-	/**
-	 * Holds the valid forms, and their
-	 * associated Form Class
-	 *
-	 * @access private
-	 * @var array
-	 */
-	private $valid_forms = array(
-		'invoice' => "Pronamic\WP\Twinfield\FormBuilder\Form\Invoice",
-		'customer' => "Pronamic\WP\Twinfield\FormBuilder\Form\Customer"
-	);
-
-	/**
-	 * Holds the valid forms, form view file
-	 *
-	 * @access private
-	 * @var array
-	 */
-	private $valid_forms_views = array(
-		'invoice' => 'create_form_invoice',
-		'customer' => 'create_form_customer'
-	);
 
 	/**
 	 * Adds a hook for the admin_init to listen for form submissions
@@ -48,122 +25,67 @@ class FormBuilder {
 	public function __construct() {
 		add_action( 'admin_init', array( $this, 'listen' ) );
 	}
-
+	
 	/**
-	 * Returns all valid forms that have been made and
-	 * verified to work.
-	 *
-	 * @since 0.0.1
-	 *
+	 * Shows the form from a registered form in the
+	 * FormBuilderFactory.  Pass in the unique name
+	 * for the form.
+	 * 
 	 * @access public
-	 * @return array All valid forms
+	 * @param string $twinfield_form
+	 * @return void
 	 */
-	public function get_valid_forms() {
-		return array_keys( $this->valid_forms );
-	}
-
-	/**
-	 * Returns the view file name for this form type
-	 *
-	 * @since 0.0.1
-	 *
-	 * @access public
-	 * @param string $type The key name for the form
-	 * @return boolean
-	 */
-	public function get_form_view_file_name( $type ) {
-		if ( array_key_exists( $type, $this->valid_forms_views ) )
-			return $this->valid_forms_views[$type];
-		else
-			return false;
-	}
-
-	/**
-	 * Will create a form based off the passed type
-	 *
-	 * @since 0.0.1
-	 *
-	 * @access public
-	 * @param string $type The name of the form to create
-	 * @param boolean $return Whether to return the view or send straight to browser
-	 * @return string The View of the chosen form
-	 */
-	public function create_form( $type = null, $return = false ) {
-		// Check type has been supplied or return
-		if ( ! $type && ! isset( $_GET['twinfield-form'] ) )
-			return;
-		
-		if ( ! $type )
-			$type = $_GET['twinfield-form'];
-
-		// Check the type is a valid form or return
-		if ( ! array_key_exists( $type, $this->valid_forms ) )
-			return;
-
-		// Get the class of the chosen type
-		$form = $this->get_class_from_type( $type );
-
-		// Generate a nonce
-		$nonce = wp_nonce_field( 'twinfield_form_builder', 'twinfield_form_nonce', true, false );
-
-		// Get the view file name
-		$viewFile = $this->get_form_view_file_name( $type );
-		
-		// Prepare the view
-		$view = new View( PRONAMIC_TWINFIELD_FOLDER . '/views/Pronamic/WP/FormBuilder' );
-		$view
-			->setVariable( 'nonce', $nonce )
-			->setVariable( $type, $form->fill_class( $_POST ) )
-			->setVariable( 'form_extra', $form->extra_variables() )
-			->setView( $viewFile );
-
-		// Determine if it should return or show the view
-		if ( $return ) {
-			return $view->retrieve();
-		} else {
-			$view->render();
+	public function show_form( $twinfield_form ) {
+		// Check the twinfield form has been registered
+		if ( $form = FormBuilderFactory::get_form( $twinfield_form ) ) {
+						
+			// Render the view
+			$form->render( $_POST );
 		}
 	}
-
+	
+	/**
+	 * Listens to every admin page request to determine if a form
+	 * from the FormBuilder has been submitted.
+	 * 
+	 * @todo decouple the relation with INPUT_GET and a set key
+	 * 
+	 * @access public
+	 * @return void
+	 */
 	public function listen() {
-		if ( ! isset( $_GET['twinfield-form'] ) )
+		if ( ! filter_has_var( INPUT_GET, 'twinfield-form' ) )
 			return;
-
-		$type = $_GET['twinfield-form'];
-
-		if ( ! array_key_exists( $type, $this->valid_forms ) )
-			return;
-
+		
 		if ( empty( $_POST ) || ! isset( $_POST['twinfield_form_nonce' ] ) )
 			return;
 
 		if ( ! wp_verify_nonce( $_POST['twinfield_form_nonce'], 'twinfield_form_builder' ) )
 			return;
-
-		$class = $this->get_class_from_type( $type );
-
+		
+		// Get the form name from the INPUT_GET input
+		// @todo should be decoupled
+		$twinfield_form = filter_input( INPUT_GET, 'twinfield-form', FILTER_SANITIZE_STRING );
+		
+		// Get the form instance
+		$form = FormBuilderFactory::get_form( $twinfield_form );
+		
+		// Attempt the submit of this form
+		$success = $form->submit( $_POST );
+		
+		// Get the notice class
 		$notice = new \ZFramework\Util\Notice();
-		if ( true === $class->submit() ) {
-			$notice->updated('Successful');
+		
+		// Determine the success response of the submission
+		if ( true === $success ) {
+			
+			$notice->updated( 'Successful' );
+			
 		} else {
 			
-			foreach ( $class->get_response()->getErrorMessages() as $error ) {
-				$notice->error($error);
+			foreach ( $form->get_response()->getErrorMessages() as $error ) {
+				$notice->error( $error );
 			}
 		}
 	}
-
-	private function get_class_from_type( $type ) {
-		try {
-
-			$reflectionClass = new \ReflectionClass( $this->valid_forms[$type] );
-			return $reflectionClass->newInstance();
-
-		} catch( \Exception $e ) {
-			return false;
-		}
-	}
-
-
-
 }
