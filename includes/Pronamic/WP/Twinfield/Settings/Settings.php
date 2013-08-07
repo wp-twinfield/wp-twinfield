2f3227f1645f154c5d2e59d81c356ff3ff80cc16
Remco Tolsma <info@remcotolsma.nl>
2013-08-07 11:34:38 +0200

    Removed some spacing.

--- includes/Pronamic/WP/Twinfield/Settings/Settings.php
+++ includes/Pronamic/WP/Twinfield/Settings/Settings.php
@@ -3,22 +3,22 @@
 namespace Pronamic\WP\Twinfield\Settings;
 
 class Settings {
-		
+
 	public function register_settings() {
-		
+
 		/**
 		 * ======
 		 * API Section
 		 * ======
 		 */
-		
+
 		add_settings_section(
 			'api',
 			__( 'API Settings', 'twinfield' ),
 			array( $this, 'section_view' ),
 			'twinfield-settings'
 		);
-		
+
 		add_settings_field(
 			'twinfield_username',
 			__( 'Username', 'twinfield' ),
@@ -27,7 +27,7 @@ class Settings {
 			'api',
 			array( 'label_for' => 'twinfield_username' )
 		);
-		
+
 		add_settings_field(
 			'twinfield_password',
 			__( 'Password', 'twinfield' ),
@@ -36,7 +36,7 @@ class Settings {
 			'api',
 			array( 'label_for' => 'twinfield_password' )
 		);
-		
+
 		add_settings_field(
 			'twinfield_organisation',
 			__( 'Organisation', 'twinfield' ),
@@ -45,7 +45,7 @@ class Settings {
 			'api',
 			array( 'label_for' => 'twinfield_organisation' )
 		);
-		
+
 		add_settings_field(
 			'twinfield_office_code',
 			__( 'Office Code', 'twinfield' ),
@@ -54,26 +54,26 @@ class Settings {
 			'api',
 			array( 'label_for' => 'twinfield_office_code' )
 		);
-		
+
 		// Registered settings for the api section
 		register_setting( 'wp_twinfield_api', 'twinfield_username' );
 		register_setting( 'wp_twinfield_api', 'twinfield_password' );
 		register_setting( 'wp_twinfield_api', 'twinfield_organisation' );
 		register_setting( 'wp_twinfield_api', 'twinfield_office_code' );
-		
+
 		/**
 		 * =========
 		 * Permalinks Section
 		 * ==========
 		 */
-		
+
 		add_settings_section(
 			'permalinks',
 			__( 'Permalink Settings', 'twinfield' ),
 			array( $this, 'section_view' ),
 			'twinfield-settings'
 		);
-		
+
 		add_settings_field(
 			'wp_twinfield_invoice_slug',
 			__( 'Invoice Slug', 'twinfield' ),
@@ -82,7 +82,7 @@ class Settings {
 			'permalinks',
 			array( 'label_for' => 'wp_twinfield_invoice_slug' )
 		);
-		
+
 		add_settings_field(
 			'wp_twinfield_customer_slug',
 			__( 'Customer Slug', 'twinfield' ),
@@ -91,51 +91,51 @@ class Settings {
 			'permalinks',
 			array( 'label_for' => 'wp_twinfield_customer_slug' )
 		);
-		
+
 		// Settings for the permalinks section
 		register_setting( 'wp_twinfield_api', 'wp_twinfield_invoice_slug' );
 		register_setting( 'wp_twinfield_api', 'wp_twinfield_customer_slug' );
 	}
-	
+
 	public function section_view() {}
-	
+
 	public function render_text( $attributes ) {
 		?>
 		<input id="<?php echo $attributes['label_for']; ?>" type="text" name="<?php echo $attributes['label_for']; ?>" value="<?php echo get_option( $attributes['label_for'] ); ?>"/>
 		<?php if ( isset( $attributes['description'] ) ) : ?><span class="description"><?php echo $attributes['description']; ?></span><?php endif; ?>
 			<?php
 	}
-	
+
 	public function render_password( $attributes ) {
 		?>
 		<input id="<?php echo $attributes['label_for']; ?>" type="password" name="<?php echo $attributes['label_for']; ?>" value="<?php echo get_option( $attributes['label_for'] ); ?>"/>
 		<?php if ( isset( $attributes['description'] ) ) : ?><span class="description"><?php echo $attributes['description']; ?></span><?php endif; ?>
 		<?php
 	}
-	
+
 	public static function tab_html( $section, $default = false ) {
-		
-		printf( 
+
+		printf(
 			"<a class='nav-tab %s' href='%s'>%s</a>",
 			( self::is_active_tab( $section ) || ( ! filter_has_var( INPUT_GET, 'tab' ) && 'api' === $section ) ) ? 'nav-tab-active' : '',
 			add_query_arg( array( 'page' => 'twinfield-settings', 'tab' => $section ), admin_url( 'admin.php' ) ),
 			ucfirst( $section )
 		);
-		
+
 	}
-	
+
 	public static function is_active_tab( $section ) {
 		return ( $section === filter_input( INPUT_GET, 'tab', FILTER_SANITIZE_STRING ) );
 	}
-	
+
 	public static function active_tab_group() {
 		$tab = self::active_tab();
-		
+
 		return 'wp_twinfield_' . $tab ;
 	}
-	
+
 	public static function active_tab() {
 		return ( $tab = filter_input( INPUT_GET, 'tab', FILTER_SANITIZE_STRING ) ) ? $tab : 'api';
 	}
-	
+
 }
