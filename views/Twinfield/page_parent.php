f1550d5d733d7120b8335ebd9fb2b7634ac7be57
Remco Tolsma <info@remcotolsma.nl>
2013-08-07 14:26:59 +0200

    Created an nicer Twinfield parent page.

--- views/Twinfield/page_parent.php
+++ views/Twinfield/page_parent.php
@@ -4,13 +4,29 @@
 
     <h2><?php echo get_admin_page_title(); ?></h2>
 
-    <form action="https://login.twinfield.com/default.aspx" method="post" target="_blank">
-        <p><?php _e( 'Below you can be taken directly to the Twinfield site.', 'twinfield' ); ?></p>
-        <p>
-            <input name="txtUserID" type="hidden" value="<?php echo $twinfield_config->getUsername(); ?>" />
-            <input name="txtPassword" type="hidden" value="<?php echo $twinfield_config->getPassword(); ?>" />
-            <input name="txtcompanyID" type="hidden" value="<?php echo $twinfield_config->getOrganisation(); ?>" />
-            <?php submit_button( __( 'Login', 'twinfield' ), 'primary', 'btnLogin', false ); ?>
-        </p>
-    </form>
+    <div id="dashboard-widgets-wrap">
+		<div id="dashboard-widgets" class="metabox-holder columns-2">
+			<div id="postbox-container-1" class="postbox-container">
+				<div class="postbox">
+					<h3 class="hndle"><span><?php _e( 'Quick Login', 'twinfield' ); ?></span></h3>
+
+					<div class="inside">
+					    <form action="https://login.twinfield.com/default.aspx" method="post" target="_blank">
+					        <p>
+					        	<a href="https://login.twinfield.com/" target="_blank">https://login.twinfield.com/</a>
+
+					            <input name="txtUserID" type="hidden" value="<?php echo $twinfield_config->getUsername(); ?>" />
+					            <input name="txtPassword" type="hidden" value="<?php echo $twinfield_config->getPassword(); ?>" />
+					            <input name="txtcompanyID" type="hidden" value="<?php echo $twinfield_config->getOrganisation(); ?>" />
+
+					            <?php submit_button( __( 'Login', 'twinfield' ), 'primary', 'btnLogin', false ); ?>
+					        </p>
+					    </form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </div>
