a057846c08f3ffa80790a5eaf0818785805d05c7
Remco Tolsma <info@remcotolsma.nl>
2014-11-10 16:52:33 +0100

    Simplified the invoice meta box, removed all AJAX stuff.

--- classes/Pronamic/WP/Twinfield/Invoice/InvoiceMetaBox.php
+++ classes/Pronamic/WP/Twinfield/Invoice/InvoiceMetaBox.php
@@ -40,8 +40,31 @@ class InvoiceMetaBox {
      */
     public function __construct() {
         add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
+
         add_action( 'save_post', array( $this, 'save' ), 10, 2 );
-        add_action( 'wp_ajax_twinfield_invoice_metabox_sync', array( $this, 'ajax' ) );
+        add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+    }
+
+    public function save_post( $post_id, $post ) {
+		if ( filter_has_var( INPUT_POST, 'twinfield_create_invoice' ) ) {
+			if ( InvoiceMetaBoxFactory::supported( $post->post_type ) ) {
+            	$synchronizer = InvoiceMetaBoxFactory::get( $post->post_type );
+
+            	$data_proxy = $synchronizer->prepare_sync( $post_id, $customer_id, $invoice_id, $invoice_type );
+
+            	$state = $data_proxy->submit();
+
+            	update_post_meta( $post_id, '_twinfield_response', $data_proxy->get_response()->getResponseDocument()->saveXML() );
+
+            	if ( true === $state ) {
+            		$invoice = \Pronamic\Twinfield\Invoice\Mapper\InvoiceMapper::map( $data_proxy->get_response() );
+
+            		update_post_meta( $post_id, '_twinfield_invoice_id', $invoice->getInvoiceNumber() );
+            		update_post_meta( $post_id, '_twinfield_customer_id', $invoice->getCustomer()->getID() );
+            		update_post_meta( $post_id, '_twinfield_invoice_type', $invoice->getInvoiceType() );
+				}
+			}
+		}
     }
 
     /**
@@ -85,6 +108,7 @@ class InvoiceMetaBox {
         global $twinfield_plugin;
 
         $twinfield_plugin->display( 'views/meta-box-invoice.php', array(
+        	'post_id'      => $post->ID,
         	'invoice_id'   => $invoice_id,
         	'customer_id'  => $customer_id,
         	'invoice_type' => $invoice_type,
@@ -102,117 +126,16 @@ class InvoiceMetaBox {
      * @return void
      */
     public function save( $post_id, \WP_Post $post ) {
-        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
             return;
+        }
 
+        $invoice_id   = filter_input( INPUT_POST, 'invoice_id', FILTER_SANITIZE_NUMBER_INT );
         $customer_id  = filter_input( INPUT_POST, 'twinfield_customer_id', FILTER_SANITIZE_NUMBER_INT );
         $invoice_type = filter_input( INPUT_POST, 'twinfield_invoice_type', FILTER_SANITIZE_STRING );
 
-        if ( ! $invoice_id = $this->check_for_invoice_id( $post_id ) )
-            $invoice_id = filter_input( INPUT_POST, 'invoice_id', FILTER_SANITIZE_NUMBER_INT );
-
         update_post_meta( $post_id, '_twinfield_invoice_id', $invoice_id );
         update_post_meta( $post_id, '_twinfield_customer_id', $customer_id );
         update_post_meta( $post_id, '_twinfield_invoice_type', $invoice_type );
-
-        if ( InvoiceMetaBoxFactory::supported( $post->post_type ) ) {
-            $synchronizer = InvoiceMetaBoxFactory::get( $post->post_type );
-            $data_proxy = $synchronizer->prepare_sync( $post_id, $customer_id, $invoice_id, $invoice_type );
-
-            $this->sync( $post_id, $data_proxy );
-        }
-    }
-
-    /**
-     * Handles the ajax callback when the action 'twinfield_invoice_metabox_sync' is triggered.
-     * Will do a final check that the post type is supported and then make the synchronization
-     * attempt.
-     *
-     * @acccess public
-     * @return void
-     */
-    public function ajax() {
-        if ( ! filter_has_var( INPUT_POST, 'post_id' ) )
-            echo json_encode( array( 'ret' => false ) );
-
-        $post_id      = filter_input( INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT );
-        $customer_id  = filter_input( INPUT_POST, 'customer_id', FILTER_SANITIZE_NUMBER_INT );
-        $invoice_type = filter_input( INPUT_POST, 'invoice_type', FILTER_SANITIZE_STRING );
-
-        if ( ! $invoice_id = $this->check_for_invoice_id( $post_id ) )
-            $invoice_id = filter_input( INPUT_POST, 'invoice_id', FILTER_SANITIZE_NUMBER_INT );
-
-        update_post_meta( $post_id, '_twinfield_invoice_id', $invoice_id );
-        update_post_meta( $post_id, '_twinfield_customer_id', $customer_id );
-        update_post_meta( $post_id, '_twinfield_invoice_type', $invoice_type );
-
-        $post = get_post( $post_id );
-
-        if ( InvoiceMetaBoxFactory::supported( $post->post_type ) ) {
-            $synchronizer = InvoiceMetaBoxFactory::get( $post->post_type );
-            $data_proxy = $synchronizer->prepare_sync( $post_id, $customer_id, $invoice_id, $invoice_type );
-
-            $this->sync( $post_id, $data_proxy );
-        }
-    }
-
-    /**
-     * Submits the FormBuilder Invoice form and calls the successful
-     * method if the state returned was true.
-     *
-     * Will echo out messages if an ajax request.
-     *
-     * @access public
-     * @param int $post_id
-     * @param \Pronamic\WP\Twinfield\FormBuilder\Form\Invoice $form_invoice
-     */
-    public function sync( $post_id, FormInvoice $form_invoice ) {
-        $state = $form_invoice->submit();
-
-        if ( true === $state ) {
-            $invoice = \Pronamic\Twinfield\Invoice\Mapper\InvoiceMapper::map( $form_invoice->get_response() );
-            $this->successful( $post_id, $invoice );
-        }
-
-        if ( is_ajax() ) {
-            if ( true === $state ) {
-                echo json_encode( array(
-                    'ret' => true,
-                    'msg' => __( 'Successfully synced', 'twinfield' )
-                ) );
-            } else {
-                echo json_encode( array(
-                    'ret'  => false,
-                    'msgs' => $form_invoice->get_response()->getErrorMessages()
-                ) );
-            }
-
-            exit;
-        }
-    }
-
-    /**
-     * Takes the Invoice entity and sets the post meta values from the
-     * response.
-     *
-     * @access public
-     * @param int $post_id
-     * @param \Pronamic\Twinfield\Invoice\Invoice $invoice
-     */
-    public function successful( $post_id, InvoiceEntity $invoice ) {
-        update_post_meta( $post_id, '_twinfield_invoice_id', $invoice->getInvoiceNumber() );
-        update_post_meta( $post_id, '_twinfield_customer_id', $invoice->getCustomer()->getID() );
-        update_post_meta( $post_id, '_twinfield_invoice_type', $invoice->getInvoiceType() );
-    }
-
-    /**
-     * Returns the _twinfield_invoice_id if exists, or false if not.
-     *
-     * @access private
-     * @param int $post_id
-     * @return string|false
-     */
-    private function check_for_invoice_id( $post_id ) {
-        return get_post_meta( $post_id, '_twinfield_invoice_id', true );
     }
 }
--- views/meta-box-invoice.php
+++ views/meta-box-invoice.php
@@ -1,3 +1,10 @@
+<?php
+
+global $post;
+
+$twinfield_respone = get_post_meta( $post->ID, '_twinfield_response', true );
+
+?>
 <script type="text/javascript">
 	jQuery( function() {
 		WP_Twinfield_Sync.invoice.ready();
@@ -9,41 +16,43 @@
 <table class="form-table">
 	<tr>
 		<th>
-			<label for="TwinfieldInvoiceMetaBoxSync_InvoiceID"><?php _e( 'Invoice ID', 'twinfield' ); ?></label>
+			<label for="twinfield_invoice_id"><?php _e( 'Invoice ID', 'twinfield' ); ?></label>
 		</th>
 		<td>
-			<input id="TwinfieldInvoiceMetaBoxSync_InvoiceID" type="text" name="twinfield_invoice_id" value="<?php echo $invoice_id; ?>" />
+			<input id="twinfield_invoice_id" type="text" name="twinfield_invoice_id" value="<?php echo esc_attr( $invoice_id ); ?>" />
 		</td>
 	</tr>
 	<tr>
 		<th>
-			<label for="TwinfieldInvoiceMetaBoxSync_CustomerID"><?php _e( 'Customer ID', 'twinfield' ); ?></label>
+			<label for="twinfield_customer_id"><?php _e( 'Customer ID', 'twinfield' ); ?></label>
 		</th>
 		<td>
-			<input id="TwinfieldInvoiceMetaBoxSync_CustomerID" type="text" name="twinfield_customer_id" value="<?php echo $customer_id; ?>" />
+			<input id="twinfield_customer_id" type="text" name="twinfield_customer_id" value="<?php echo esc_attr( $customer_id ); ?>" />
 		</td>
 	</tr>
 	<tr>
 		<th>
-			<label for="TwinfieldInvoiceMetaBoxSync_InvoiceType"><?php _e( 'Invoice Type', 'twinfield' ); ?></label>
+			<label for="twinfield_invoice_type"><?php _e( 'Invoice Type', 'twinfield' ); ?></label>
 		</th>
 		<td>
-			<input id="TwinfieldInvoiceMetaBoxSync_InvoiceType" type="text" name="twinfield_invoice_type" value="<?php echo $invoice_type; ?>" />
+			<input id="twinfield_invoice_type" type="text" name="twinfield_invoice_type" value="<?php echo esc_attr( $invoice_type ); ?>" />
 		</td>
 	</tr>
 </table>
+
+<?php if ( $twinfield_respone ) : ?>
+
+	<pre><?php echo htmlspecialchars( $twinfield_respone ); ?></pre>
+
+<?php endif; ?>
 
 <?php if ( $invoice_id ) : ?>
 
     <a class="button" target="_blank" href="<?php echo twinfield_admin_view_invoice_link( $invoice_id ); ?>"><?php _e( 'View' ); ?></a>
 
-<?php endif; ?>
+<?php elseif ( $is_supported ) : ?>
 
-<?php if ( $is_supported ) : ?>
-
-    <input id="TwinfieldInvoiceMetaBoxSync_SyncButton" class="button button-primary" type="submit" value="<?php _e( 'Sync', 'twinfield' ); ?>"/>
-
-    <span id="TwinfieldInvoiceMetaBoxSync_SpinnerHolder"></span>
+	<?php submit_button( __( 'Create Invoice', 'twinfield' ), 'secondary', 'twinfield_create_invoice', false ); ?>
 
 <?php else : ?>
 
