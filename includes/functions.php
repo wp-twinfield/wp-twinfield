06df3106234a5463b5db0914b6b6650bb4b5bc04
Remco Tolsma <info@remcotolsma.nl>
2014-11-12 16:37:43 +0100

    Removed unused function.

--- includes/functions.php
+++ includes/functions.php
@@ -36,31 +36,6 @@ function twinfield_get_debtor( $id ) {
 
 }
 
-function wp_twinfield_admin_query_nav( $ignore_trigger = '' ) {
-	$selected_tab = filter_input( INPUT_GET, 'tab', FILTER_SANITIZE_STRING );
-
-	$tabs = array(
-		'customer' => array(
-			'title' => __( 'Customer', 'twinfield' ),
-			'link'  => twinfield_admin_view_customer_link(),
-		),
-		'invoice' => array(
-			'title' => __( 'Invoice', 'twinfield' ),
-			'link'  => twinfield_admin_view_invoice_link(),
-		),
-	);
-
-	if ( ! filter_has_var( INPUT_GET, $ignore_trigger ) ) : ?>
-
-		<h2 class="nav-tab-wrapper">
-			<?php foreach ( $tabs as $tab => $att ) : ?>
-				<a class="nav-tab <?php if ( $tab === $selected_tab ): ?>nav-tab-active<?php endif; ?>" href="<?php echo esc_attr( $att['link'] ); ?>"><?php echo esc_html( $att['title'] ); ?></a>
-			<?php endforeach; ?>
-		</h2>
-
-	<?php endif;
-}
-
 function twinfield_admin_view_customer_link( $customer_id = false ) {
 	$query_args = array(
 		'page'                  => 'twinfield_customers',
